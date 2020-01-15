59a10fe9d155f09c17ab8302e2ede5a90a5e3941
vajiral <vajee555@gmail.com>
2020-01-16 11:30:27 +1300

    Initial commit.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,4 @@
+# Contributions Welcome!
+
+Pull Requests and Community Contributions are the bread and butter of open source software. Every contribution- from bug
+reports to feature requests, typos to full new features- are greatly appreciated.
---
+++ autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the ImapFetch package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+spl_autoload_register(function ($class) {
+    $base = '/src/ImapFetch';
+
+    $file = __DIR__.$base.strtr($class, '\\', '/').'.php';
+    if (file_exists($file)) {
+        require $file;
+
+        return true;
+    }
+});
